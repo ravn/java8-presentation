2beb33561efbfdabd1249fdfae7e38633562229a
Thorbjoern Ravn Andersen <thorbjoern@gmail.com>
2016-01-05 16:46:48 -0700

    got started on streams

--- md/streams.md
+++ md/streams.md
@@ -22,5 +22,99 @@ a traditional for-loop.  Intellij knows how to convert the iterator-while to a f
             doStuff(s);
         }
 
+streams (Java 8 - 2014) - 
+
+        l.stream().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String e) {
+                doStuff(e);
+            }
+        });
+
+or
+
+        foo.stream().forEach(e -> doStuff(e));
+        
+Note that the code now works with one element at a time and the actual loops are hidden 
+in the library routines.
+
+
+Producers:
+---
+
+Most collections have had the `stream()` method added, but an infinite FIXME can be implemented.  
+
+FIXME: int sum = Arrays.stream(myIntArray).sum();
+
+Collecting:
+---
+A collector is responsible for collecting the elements in the stream into a datastructure
+which is then returned.  This is typically a List, a Collection or joined into a String.
+
+    List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
+    System.out.println(la.stream().map(s -> s.startsWith("a")).collect(Collectors.toList()));
+    // [true, false, false, true]
+    System.out.println(la.stream().collect(Collectors.joining(", ")));
+    // ad, bc, ba, ac
+
+    Map<String, String> m = la.stream().collect(
+            Collectors.groupingBy(e -> e.substring(0,1),
+            Collectors.joining("+")));
+    System.out.println(Arrays.toString(m.entrySet().toArray()));
+    // [a=ad+ac, b=bc+ba]
+
+Static imports may shorten this a bit.
+
+
+Selecting elements:
+---
+
+The `filter(java.util.function.Predicate<T>)` method on a Stream, filters out those
+elements for which the predicate returns false.
+
+    List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
+    la.stream().filter(s -> s.startsWith("a")).forEach(e -> System.out.println(e));
+    // ad
+    // ac
+    
+Note that the `e->System.out.println(e)` lambda expression can be replaced with the
+`System.out::println` method reference, like this:
+
+    la.stream().filter(s -> s.startsWith("a")).forEach(System.out::println);
+    
+FIXME: TakeAny, TakeOne. FIXME: HEAD?
+
+
+Transforming:
+---
+The `map(Function<T,R>)` allows for converting a `Stream<T>` to a `Stream<R>`.
+
+
+
+Sorting:
+---
+
+The `sorted()` method allows for sorting the stream elements at this point, either 
+according to their natural order if `Comparable` or by providing a Comparator.
+
+Note that this will cause all elements from upstream to be collected here and sorted, before 
+providing the sorted elements again downstream breaking the lazy evaluation.
+
+
+
+
+Peeking:
+---
+You may need to just _look_ at an element for any reason, usually debugging.  Use
+the `peek(Consumer<? super T> action)` method for this.
+
+
 
 Use `peek(System.out::println)` to peek FIXME. 
+
+
+
+
+
+
+
--- src/dk/statsbiblioteket/user/tra/java8/Main.java
+++ src/dk/statsbiblioteket/user/tra/java8/Main.java
@@ -4,6 +4,7 @@ import java.text.Collator;
 import java.util.*;
 import java.util.function.Function;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * MAIN JAVADOC
@@ -49,23 +50,44 @@ public class Main {
         l.sort(Comparator.comparing(String::length));
         System.out.println(l); // [a, Bc, abc]
 
-        List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
-        // http://stackoverflow.com/a/24442897/53897
-        // http://blog.jooq.org/2014/01/31/java-8-friday-goodies-lambdas-and-sorting/
-        la.sort(Comparator.comparing((String e) -> e.substring(1,2))
-                .thenComparing(e -> e.substring(0,1)));
-        System.out.println(la); // [ba, ac, bc, ad]
-        la.sort(Comparator.comparing((String e) -> e.substring(1,2))
-                .thenComparing(e -> e.substring(0,1)).reversed());
-        System.out.println(la); // [ad, bc, ac, ba]
+        {
+            List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
+            // http://stackoverflow.com/a/24442897/53897
+            // http://blog.jooq.org/2014/01/31/java-8-friday-goodies-lambdas-and-sorting/
+            la.sort(Comparator.comparing((String e) -> e.substring(1, 2))
+                    .thenComparing(e -> e.substring(0, 1)));
+            System.out.println(la); // [ba, ac, bc, ad]
+            la.sort(Comparator.comparing((String e) -> e.substring(1, 2))
+                    .thenComparing(e -> e.substring(0, 1)).reversed());
+            System.out.println(la); // [ad, bc, ac, ba]
 
-        System.out.println(String.join(" ", "Hello", "World"));
+            System.out.println(String.join(" ", "Hello", "World"));
 
-        Logger log = Logger.getLogger("foo");
-        Function<String, String> f = s -> { log.fine(s); return s; };
+            Logger log = Logger.getLogger("foo");
+            Function<String, String> f = s -> {
+                log.fine(s);
+                return s;
+            };
+        }
+        l.stream().forEach(e -> doStuff(e));
+        {
+            List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
+            la.stream().filter(s -> s.startsWith("a")).forEach(System.out::println);
+        }
+
+        {
+            List<String> la = Arrays.asList("ad", "bc", "ba", "ac");
+            System.out.println(la.stream().map(s -> s.startsWith("a")).collect(Collectors.toList()));
+            System.out.println(la.stream().collect(Collectors.joining(", ")));
+            Map<String, String> m = la.stream().collect(
+                    Collectors.groupingBy(e -> e.substring(0,1),
+                    Collectors.joining("+")));
+            System.out.println(Arrays.toString(m.entrySet().toArray()));
+        }
     }
 
-    public static void doStuff(String s) {
-        System.out.println(s);
+
+    public static String doStuff(String s) {
+        return ">" + s + "<";
     }
 }
